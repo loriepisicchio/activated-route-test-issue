924647035b1c424a5a06663a7dfeacd636592cc3
Lorie Pisicchio <lorie.pisicchio@gmail.com>
2020-11-09 17:05:31 +0100

    Show issue in tests with both ActivatedRoute and Router mock

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,67 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {createComponentFactory, mockProvider, Spectator} from "@ngneat/spectator/jest";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRouteStub, SpectatorOverrides} from "@ngneat/spectator";
+import {TestBed} from "@angular/core/testing";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    imports: [RouterTestingModule],
+    providers: [{provide: ActivatedRoute, useValue: new ActivatedRouteStub({queryParams: {shouldCreateAccount: 'true'}})},
+      mockProvider(Router),
+    ]
+  });
+
+
+  const createComponentWithMockCookie = (cookie: string, options?: SpectatorOverrides<AppComponent>) => {
+    try {
+      jest.spyOn(document, 'cookie', 'get').mockReturnValue(cookie);
+      return createComponent(options);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  beforeEach(() => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    spectator = createComponentWithMockCookie('');
+    expect(spectator.component).toBeTruthy();
   });
 
   it(`should have as title 'activated-route-test-issue'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    spectator = createComponentWithMockCookie('cookie');
+    const app = spectator.component;
     expect(app.title).toEqual('activated-route-test-issue');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
+    spectator = createComponentWithMockCookie('cookie');
+    const compiled = spectator.element;
     expect(compiled.querySelector('.content span').textContent).toContain('activated-route-test-issue app is running!');
   });
+
+  it('should render params in route', () => {
+    const spyRouter = jest.spyOn(TestBed.inject(Router), 'navigate').mockReturnValue(Promise.resolve(true));
+    spectator = createComponentWithMockCookie('cookie');
+    const compiled = spectator.element;
+    expect(compiled.querySelector('.activated-route').textContent).toContain('{"shouldCreateAccount":"true"}');
+  })
+
+  it('we should be able to test with different value for activatedRoute', () => {
+    const spyRouter = jest.spyOn(TestBed.inject(Router), 'navigate').mockReturnValue(Promise.resolve(true));
+
+    spectator = createComponentWithMockCookie('cookie', {
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: new ActivatedRouteStub({queryParams: {shouldCreateAccount: 'false'}})
+      }]
+    });
+    const compiled = spectator.element;
+    expect(compiled.querySelector('.activated-route').textContent).toContain('{"shouldCreateAccount":"false"}');
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'activated-route-test-issue';
+  content: string;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.content = JSON.stringify(this.activatedRoute.snapshot.queryParams);
+  }
 }
